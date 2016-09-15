647dcbdc9dca769120beb08ba80e703198896159
286600136@qq.com <zhmlvft.1314>
2016-09-15 11:23:27 +0800

    init

--- oauth2-server/src/main/java/com/trinasolar/oauth2/server/Oauth2ServerApplication.java
+++ oauth2-server/src/main/java/com/trinasolar/oauth2/server/Oauth2ServerApplication.java
@@ -2,9 +2,7 @@ package com.trinasolar.oauth2.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 /**
  * Created by zhm on 16-9-13.
